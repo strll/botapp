ea4d2e9b1cc408fb85e0b7fc002b2fc6e675edb5
whlexv <whlexv@163.com>
2023-03-12 20:52:23 +0800

    修改'这是今天的新闻'页面

--- pages/bot/News.vue
+++ pages/bot/News.vue
@@ -1,14 +1,16 @@
 <template>
-	<view>
+	<view class="newsBox">
 		<view class="myimg">
 			<image :src="newstitle.url"></image>
 		</view>
 		<view v-text="newstitle.msg" class="mytext"></view>
-		<view v-for="objs in list">
-			<!-- 	<view v-text="objs.title" class="mytext"></view> -->
-			<u--text prefixIcon="zhihu" iconStyle="font-size: 19px" :text="objs.title"></u--text>
-			<view v-for=" obj in objs.img" class="myimg">
-				<u--image :showLoading="true" :src="obj"></u--image>
+		<view class="listBox">
+			<view v-for="objs in list" class="listItem">
+				<!-- 	<view v-text="objs.title" class="mytext"></view> -->
+				<u--text prefixIcon="zhihu" iconStyle="font-size: 19px" :text="objs.title"></u--text>
+				<view v-for=" obj in objs.img" class="myimg itemImg">
+					<u--image :showLoading="true" :src="obj"></u--image>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -71,7 +73,7 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.myimg {
 		display: flex;
 
@@ -82,11 +84,33 @@
 
 	.mytext {
 
-
+		font-weight: bold;
+		padding: 0 10rpx;
+		margin: 10rpx 0;
 		text-indent: 2em;
 		font-family: "微软雅黑";
 		font-size: 30rpx;
 		line-height: 26px;
 		line-height: 35rpx;
+	}
+	
+	.newsBox{
+		background-color: white;
+	}
+	
+	.listBox{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		.listItem{
+			width: 90%;
+			padding: 10rpx 15rpx 20rpx;
+			margin-bottom: 20rpx;
+			background-color: $uni-bg-color-grey;
+			border-radius: 10rpx;
+			.itemImg{
+				margin-top: 20rpx;
+			}
+		}
 	}
 </style>
